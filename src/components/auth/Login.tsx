2667c36459d405777e17877a5782394b2b1f71d8
ctola91 <ctolapacheco@gmail.com>
2022-11-10 23:03:05 -0400

    add Login implementation

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, FormEventHandler } from 'react'
+import { useAuth } from '../../hooks/useAuth';
+import { useState } from 'react';
 
 type Props = {}
 
 const Login = (props: Props) => {
+    const { signIn } = useAuth();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        signIn({ email, password });
+    }
+
+    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { value, name },
+        } = event;
+        if (name === 'email') {
+            setEmail(value);
+        }
+        if (name === 'password') {
+            setPassword(value);
+        }
+    }
+
     return (
-        <div>Login</div>
+        <>
+            <div>Login</div>
+            <form onSubmit={handleOnSubmit}>
+                <label>Email</label>
+                <input type="text" value={email} name="email" onChange={handleOnChange} />
+                <label>Password</label>
+                <input type="password" value={password} name="email" />
+                <input type="submit" value="Login" />
+            </form>
+        </>
     )
 }
 
