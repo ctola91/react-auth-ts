3f0ce3e7ce360b7738d417652554021bd2bc6e3d
ctola91 <ctolapacheco@gmail.com>
2022-11-22 22:10:14 -0400

    Add menu implementation and icons

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react";
 
 import './Header.css';
+import { Link, NavLink } from "react-router-dom";
 
 const Header = (): JSX.Element => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
-        <header>
-            <div className="title"><h1>Title</h1></div>
-            <div className="menu">Menu</div>
-        </header>
+        <>
+            <header>
+                <div className="title">
+                    <Link to="/"><h1>Title</h1></Link>
+                </div>
+                <div className="menu">
+                    <button onClick={toggleMenu}><FontAwesomeIcon icon={faBars} /></button>
+                </div>
+                <div className={isOpen ? "header-menu active" : "header-menu"} >
+                    <ul>
+                        <li className="header-menu-item"><Link to="/">Home</Link></li>
+                        <li className="header-menu-item"><Link to="about">About</Link></li>
+                        <li className="header-menu-item"><Link to="signup">Signup</Link></li>
+                        <li className="header-menu-item"><Link to="login">Login</Link></li>
+                    </ul>
+                </div>
+            </header>
+
+        </>
     );
 };
 
